d5a74b8bacdea732b3ada8a41d898aba17b8de6c
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-13 03:26:47 -0700

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<unistd.h>
 
 /**
  * main - Entry point
@@ -9,7 +8,7 @@
  */
 int main(void)
 {
-	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int n = 0;
 	while (st[n] != '\0') {
 		putchar(st[n]);
