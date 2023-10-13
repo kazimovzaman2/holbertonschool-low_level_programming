a4a562a72f1c0bfa6bb02b551f43753cd7c7c301
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-12 19:30:43 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,6 +8,10 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
