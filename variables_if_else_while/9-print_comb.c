105c58b7fb7bcf84a081700094f100ca9a6052db
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-16 05:38:47 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'putchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
