8f5c684ea7aac8d6b5efb5caea29bc820ad9326c
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-16 04:58:09 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'putchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
