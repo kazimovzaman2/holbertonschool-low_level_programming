e9653bc8747dd2f44c84cd2648fc2f23311f50af
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-17 08:38:23 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: get integer value
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i, j, a, b, r;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			a = i * j;
+
+			if (j > 0 && a < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j > 0 && a >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (a >= 10)
+			{
+				r = a % 10;
+				b = a / 10;
+				_putchar(b + '0');
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar(a + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
