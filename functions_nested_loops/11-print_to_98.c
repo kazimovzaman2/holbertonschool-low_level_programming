6475da908084cda624a03059fdb5a8c5fc5d6232
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-17 09:48:46 -0700

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ * @n: get integer value
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("98\n");
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}	
+	}
+	else if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("98\n");
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+}
