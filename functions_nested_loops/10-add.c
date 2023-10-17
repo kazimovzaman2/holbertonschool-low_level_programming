e9f2abe68b8b7145d9d2a788aada2631e1540d79
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-17 08:47:45 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @n: get integer value
+ * @m: get integer value
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+
+	return (result);
+}
