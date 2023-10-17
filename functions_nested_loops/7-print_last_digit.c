2d0e1f36d065a0135730b7aa9bc672ff19537b99
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-17 06:52:51 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,17 @@
  */
 int print_last_digit(int n)
 {
-	if (n >= 0)
+	if (n < 0)
 	{
-		int result = n % 10;
-		_putchar(result + 48);
-		return (result);
+		int k = n % 10;
+		int m = k * -1;
+
+		_putchar(m + 48);
+		return (m);
 	}
-	else if (n < 0)
+	else
 	{
-		int result = -1 * n;
-		int result1 = result % 10;
-		_putchar(result1 + 48);
-		return (result1);
+		_putchar((n % 10) + 48);
+		return (n % 10);
 	}
 }
