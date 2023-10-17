89c6d2180f2406582119176558b8404855023cb9
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-17 06:05:44 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: get integer value
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	if (n >= 0)
+	{
+		int result = n % 10;
+		return (result);
+	}
+	else if (n < 0)
+	{
+		int result = -1 * n;
+		int result1 = result % 10;
+		return (result1);
+	}
+	return (0);
+}
