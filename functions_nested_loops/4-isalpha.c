52ec63aebc701c86cbd4dd79e080ae7d8513f3c9
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-17 05:22:01 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: get integer value
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c > 97 && c < 122) || (c > 65 && c < 90))
+	{
+		return (1);
+	}
+	return (0);
+}
