4301fa01f7e0bcc733fc079fb07bfb9891868951
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-18 02:12:51 -0700

    103

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'awdaw'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 50;
+	long int num1 = 1;
+	long int num2 = 2;
+	long int next = num1 + num2;
+	int total = 0;
+
+
+	while (1)
+	{
+		num1 = num2;
+		num2 = next;
+		next = num1 + num2;
+		if (next >= 40000000)
+			break;
+		total = total + next;
+	}
+
+	printf("%d\n", total);
+	return (0);
+}
