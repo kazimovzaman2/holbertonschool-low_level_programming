c5300dcdfbd4375a5c29dfdc2a8b7d97b6fc64a0
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-19 19:58:13 -0700

    6

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ *
+ * @n: 'var'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			if (i < n - 1)
+				_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
