114ff05848cb9fc5c3b083adadd849f7f0333ed2
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-20 04:09:41 -0700

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * @size: 'var'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
