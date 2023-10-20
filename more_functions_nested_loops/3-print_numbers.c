5709d0b40b06247d2964e7b1ed531505250b6ed2
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-19 19:23:27 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <=9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
