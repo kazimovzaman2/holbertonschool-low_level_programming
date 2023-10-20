9cdb3d3ddd0ae2e2b81348ee72790c416e2b7f54
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-19 19:28:09 -0700

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
