cabfcb31336d422797c47918d969e838c4a79823
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-23 06:07:24 -0700

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * rev_string - Entry point
+ *
+ * @s: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	char *a;
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		*a = *s;
+		c++;
+		s++;
+		a++;
+	}
+
+	s--;
+	for (; c > 0; c--)
+	{
+		*s = *(a - c);
+		s--;
+	}
+}
