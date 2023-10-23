e1a996cd32125bd9375f33b41876585ce811be72
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-23 05:33:59 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ *
+ * @s: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int i;
+	int size = 0;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+
+	for (i = size; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
