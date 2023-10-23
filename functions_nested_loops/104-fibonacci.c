2cd3c9b6e58fed61c60a987fd00056f592494189
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-22 19:37:52 -0700

    14

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,78 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <inttypes.h>
+/**
+ * ifs - does math operations.
+ * @a: unsigned long.
+ * @b: unsigned long.
+ * @c: int.
+ */
+void ifs(uint64_t a, uint64_t b, int c)
+{
+	uint64_t a_half1, b_half1, a_half2, b_half2, s_half1, s_half2;
 
+	if (c == 93)
+	{
+		a_half1 = a / 10000000000;
+		a_half2 = a % 10000000000;
+		b_half1 = b / 10000000000;
+		b_half2 = b % 10000000000;
+	}
+	if (c >= 93 && c <= 97)
+	{
+		s_half1 = a_half1 + b_half1;
+		s_half2 = a_half2 + b_half2;
+		if (s_half2 > 9999999999)
+		{
+			s_half1 = s_half1 + (s_half2 / 10000000000);
+			s_half2 = s_half2 - (s_half2 / 10000000000) * 10000000000;
+		}
+		a_half1 = b_half1;
+		a_half2 = b_half2;
+		b_half1 = s_half1;
+		b_half2 = s_half2;
+		printf(", %"PRIu64"%"PRIu64, s_half1, s_half2);
+	}
+	else
+	{
+		s_half1 = a_half1 + b_half1;
+		s_half2 = a_half2 + b_half2;
+		if (s_half2 > 9999999999)
+		{
+			s_half1 = s_half1 + (s_half2 / 10000000000);
+			s_half2 = s_half2 - (s_half2 / 10000000000) * 10000000000;
+		}
+		printf(", %"PRIu64"%"PRIu64, s_half1, s_half2);
+	}
+}
 /**
  * main - Entry point
  *
- * Description: 'awdaw'
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-	int64_t a, b, s;
+	uint64_t a, b, s;
 	int c;
+
 	a = 1;
 	b = 2;
 	c = 3;
-
-	printf("%lu, %lu", a, b);
+	printf("%"PRIu64", %"PRIu64, a, b);
 	while (c < 99)
 	{
-		s = a + b;
-		a = b;
-		b = s;
-		c++;
-		printf(", %lu", s);
+		if (c < 93)
+		{
+			s = a + b;
+			a = b;
+			b = s;
+			c++;
+			printf(", %"PRIu64, s);
+		}
+		else
+		{
+			ifs(a, b, c);
+			c++;
+		}
 	}
 	printf("\n");
 	return (0);
