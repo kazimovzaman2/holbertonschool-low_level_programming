ac6e3317f64f9bab4201b4d1ccc00e1781ba12b3
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-23 06:27:10 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ *
+ * @s: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int count = 0;
+	
+	while (*str != '\0')
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+		count++;
+	}
+	_putchar('\n');
+}
