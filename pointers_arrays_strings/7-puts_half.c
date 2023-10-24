b648ab213bdb9a65de1704bac1eed4d3b0e2204d
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-24 01:20:43 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ *
+ * @str: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+	int size = 0;
+	int i;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	if (size % 2 == 0)
+	{
+		for (i = size / 2; i <= size; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (size - 1) / 2; i <= size; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
