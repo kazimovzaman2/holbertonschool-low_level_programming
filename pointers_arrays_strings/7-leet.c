765b2028d60f570319b28925be8e18398189c8b8
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 07:37:29 -0700

    commited by noone

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,13 +11,14 @@ char *leet(char *bush)
 {
 	int i, j;
 	char letter[] = { 'a', 'e', 'o', 't', 'l' };
+	char letterUP[] = { 'A', 'E', 'O', 'T', 'L' };
 	char number[] = { 4, 3, 0, 7, 1 };
 
 	for (i = 0; bush[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (bush[i] == letter[j])
+			if (bush[i] == letter[j] || bush[i] == letterUP[j])
 				bush[i] = number[j] + '0';
 		}
 	}
