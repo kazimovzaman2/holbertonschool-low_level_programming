2488e9f375e9aee90962bf5766f24b145422387b
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 02:02:30 -0700

    commited by noone

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Entry point
+ *
+ * @s1: 'varaibles'
+ * @s2: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	do {
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] > s2[i])
+				return (15);
+			else
+				return (s1[i] - s2[i]);
+		}
+		i++;
+	} while (s1[i] != '\0' && s1[i] != s2[i]);
+	return (0);
+}
