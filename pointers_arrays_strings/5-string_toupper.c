e2dcb76e1f7ca4c13ec3a013b88355d6534845b7
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 04:52:06 -0700

    commited by noone

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,17 +4,21 @@
 /**
  * *string_toupper - Entry point
  *
+ * @bush: 'var'
  * Return: Always 0 (Success)
  */
 char *string_toupper(char *bush)
 {
-	int i, l = strlen(bush);
+	int i;
 
-	for (i = 0; i < l; i++)
+	for (i = 0; bush[i] != '\0'; i++)
 	{
 		int x = bush[i];
+
 		if (bush[i] >= 97 && bush[i] <= 122)
+		{
 			bush[i] = x - 32;
+		}
 	}
 
 	return (bush);
