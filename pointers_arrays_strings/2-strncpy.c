8a170547e3bba644816244b1e9cc64fdba843d43
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 01:37:52 -0700

    commited by noone

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - Entry point
+ *
+ * @dest: 'varaibles'
+ * @src: 'varaibles'
+ * @n: 'varaibles'
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		if (i == n)
+			break;
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return dest;
+}
