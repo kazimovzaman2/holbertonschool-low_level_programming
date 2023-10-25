c93c1510668af6d366fabd87e577b6359a3e0710
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 05:12:53 -0700

    commited by noone

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *cap_string - Entry point
+ *
+ * @bush: 'var'
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *bush)
+{
+	int i;
+
+	for (i = 0; bush[i] != '\0'; i++)
+	{
+
+		if (bush[i] == 32 || bush[i] == 10 || bush[i] == 9)
+		{
+			if (bush[i + 1] >= 97 && bush[i] <= 122)
+				bush[i + 1] = bush[i + 1] - 32;
+		}
+	}
+
+	return (bush);
+}
