5ead685b9d3bdc66159fc976cc4e721d2c0cebe1
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 07:30:08 -0700

    commited by noone

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *leet - Entry point
+ *
+ * @bush: 'var'
+ * Return: Always 0 (Success)
+ */
+char *leet(char *bush)
+{
+	int i, j;
+	char letter[] = { 'a', 'e', 'o', 't', 'l' };
+	char number[] = { 4, 3, 0, 7, 1 };
+
+	for (i = 0; bush[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (bush[i] == letter[j])
+				bush[i] = number[j] + '0';
+		}
+	}
+
+	return (bush);
+}
