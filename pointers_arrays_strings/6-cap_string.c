b863df2c7682fff40f3eb37a2e55701a6fbe09ed
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 05:15:27 -0700

    commited by noone

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,11 @@ char *cap_string(char *bush)
 
 	for (i = 0; bush[i] != '\0'; i++)
 	{
-
+		if (i == 0 && bush[i] >= 97 && bush[i] <= 122)
+			bush[i] = bush[i] - 32;
 		if (bush[i] == 32 || bush[i] == 10 || bush[i] == 9)
 		{
-			if (bush[i + 1] >= 97 && bush[i] <= 122)
+			if (bush[i + 1] >= 97 && bush[i + 1] <= 122)
 				bush[i + 1] = bush[i + 1] - 32;
 		}
 	}
