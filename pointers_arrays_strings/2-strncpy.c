fdbc92635c87ded431f6caadc2acf28dbb3f82f1
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-25 01:42:15 -0700

    commited by noone

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
-	{
-		if (i == n)
-			break;
-		*(dest + i) = *(src + i);
-		i++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\n';
 
