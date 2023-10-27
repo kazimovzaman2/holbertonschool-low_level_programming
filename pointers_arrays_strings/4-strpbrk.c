b8d7b20efe67f6700aa6fbe1729e3bd2842578e1
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-26 20:15:58 -0700

    commited by noone

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk -  gets the length of a prefix substring
+ * @s: checker
+ * @accept: checker
+ *
+ * Return: int.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+				return &s[i];
+			j++;
+		}
+		i++;
+	}
+	return NULL;
+}
