fd4b2777c3736abb49d5a2799894133c78efeefe
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-28 07:38:40 -0700

    commited by noone

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,21 @@
  */
 char *rot13(char *bush)
 {
-	int i;
+int i;
 
-	for (i = 0; bush[i] != '\0'; i++)
-	{
-		char base = (bush[i] >= 'a' && bush[i] <= 'z') ? 'a' : 'A';
+while (bush[i] != '\0')
+{
+for (; (bush[i] >= 65 && bush[i] <= 90) || (bush[i] >= 97 && bush[i] <= 122);)
+{
+if ((bush[i] >= 65 && bush[i] <= 77) || (bush[i] >= 97 && bush[i] <= 109))
+{
+	bush[i] = bush[i] + 26;
+}
+bush[i] = bush[i] - 13;
+break;
+}
+i++;
+}
 
-		if (bush[i] >= base && bush[i] <= base + 25)
-			bush[i] = ((bush[i] - base + 13) % 26) + base;
-	}
-	return (bush);
+return(bush);
 }
