797842f090061133fcfbc7c1cdef2f3ec6872398
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-10-30 06:14:40 -0700

    commited by noone

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+int sqrt_2(int n, int k)
+{
+	if (n == k)
+		return (-1);
+	if (n * n == k)
+		return (n);
+	return (sqrt_2(n + 1, k));
+}
+
+
+
+/**
+ * _sqrt_recursion - check the code
+ *
+ * @n: var
+ *
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return (sqrt_2(1, n));
+}
