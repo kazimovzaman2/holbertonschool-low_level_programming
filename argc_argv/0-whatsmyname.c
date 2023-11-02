9021d34dc7f2c4c15d774945ee9371da1c4b02e4
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-02 05:17:21 -0700

    commited by noone

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,9 +4,12 @@
 /**
  * main - check the code
  *
+ * @argc: var
+ * @argv: var
+ *
  * Return: Always 0.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
