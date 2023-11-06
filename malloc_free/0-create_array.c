babc3859ed7c5bab1a3c6f430e8ea5ee35e1a1dc
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-06 04:38:11 -0800

    commited by noone

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < si; i++)
 	{
 		s[i] = c;
