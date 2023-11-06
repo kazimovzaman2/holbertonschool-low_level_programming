8270dc2c8e622cf9c56073a88fe167ac217e1ab8
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-06 04:41:55 -0800

    commited by noone

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - check the code
@@ -13,14 +14,14 @@ char *_strdup(char *str)
 {
 	char *s;
 	int i;
-	int si = size;
+	int size = strlen(str);
 
 	if (size == 0)
 		return (NULL);
 	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < si; i++)
+	for (i = 0; i < size; i++)
 	{
 		s[i] = str[i];
 	}
