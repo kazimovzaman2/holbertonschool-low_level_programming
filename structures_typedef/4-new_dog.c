d30b000e2b5a555ca013ab9ffc7e8e5fc2a09fc7
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-20 01:43:34 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,13 +14,33 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int nl, ol;
 	dog_t *new;
-	new = malloc(sizeof(dog_t));
 
+	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
+
+	nl = strlen(name);
+	ol = strlen(owner);
+
+	new->name = malloc(sizeof(char) * nl);
+	if (new->name == NULL)
+	{
+		free(name);
+		return (NULL);
+	}
 	new->name = name;
+
 	new->age = age;
+
+	new->owner = malloc(sizeof(char) * ol);
+	if (new->owner == NULL)
+	{
+		free(owner);
+		return (NULL);
+	}
 	new->owner = owner;
+
 	return (new);
 }
