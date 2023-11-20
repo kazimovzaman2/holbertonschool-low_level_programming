11b7ede7ae9b133565c23d6e9e66ff1ce0c724bd
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-19 23:57:05 -0800

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * @d: var
+ * @name: var
+ * @age: var
+ * @owne: var
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
