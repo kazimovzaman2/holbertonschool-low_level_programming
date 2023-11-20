2427d1377ff8e74c8a10a821a26b1673afabc1d4
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-19 23:53:55 -0800

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
