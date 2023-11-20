a99af10b549e01aad6fafee10274b8a80cd2afb6
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-20 04:42:27 -0800

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: the integer to print
+ * @size: var
+ * @action: var
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		return;
+	
+	for(i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
