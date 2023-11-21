e24da6d6601330699bbe1c6d18d6d0cd02490985
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-21 03:32:36 -0800

    0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - check the code
+ *
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list ptr;
+
+	if (n == 0)
+		return (0);
+	
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ptr, int);
+
+	va_end(ptr);
+	return sum;
+}
