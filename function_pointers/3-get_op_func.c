590beb51c904264851709f7fdfaa0b6faa9e3464
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-20 19:56:16 -0800

    3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - prints an integer
+ *
+ * @s: var
+ *
+ * Return: Nothing.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return ops[i].f;
+		i++;
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result = 0;
+	char *op;
+
+	if (argc != 4)
+		exit(98);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (strcmp(op, "'*'") == 0)
+		op = "*";
+	else if (strcmp(op, "'%'") == 0)
+		op = "%";
+
+	switch (*op)
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			result = get_op_func(op)(num1, num2);
+			printf("%d\n", result);
+			break;
+		default:
+			printf("Error\n");
+	}
+
+	return (0);
+}
