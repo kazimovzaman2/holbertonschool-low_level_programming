d93ead6f28298f3c87a08c0530b08ddfa663b4c7
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-22 08:22:12 -0800

    100

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,9 @@
-section .data
-    hello db 'Hello, World', 0xa  ; String to be printed, 0xa is the newline character
-section .text
-    global _start
-_start:
-    ; Write the string to the standard output
-    mov rax, 1         ; System call number for sys_write
-    mov rdi, 1         ; File descriptor 1 is stdout
-    mov rsi, hello     ; Pointer to the string
-    mov rdx, 13        ; Length of the string
-    syscall
-    ; Exit the program
-    mov rax, 60        ; System call number for sys_exit
-    xor rdi, rdi       ; Exit code 0
-    syscall
+global  main
+        extern printf
+main:
+        mov     edi, format
+        xor     eax, eax
+        call    printf
+        mov     eax, 0
+        ret
+format: db `H`,0
