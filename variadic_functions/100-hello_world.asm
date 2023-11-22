64abcdbec929fa205ca628f9eed122b81dcaaf4e
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-22 08:40:16 -0800

    100

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,9 +1,17 @@
-global  main
-        extern printf
+section .data
+    msg db "Hello world",10      ; 10 is the ASCII code for a new line (LF)
+
+global main
+
+section .text
+
 main:
-        mov     edi, format
-        xor     eax, eax
-        call    printf
-        mov     eax, 0
-        ret
-format: db `H`,0
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 13
+    syscall
+    
+    mov rax, 60
+    mov rdi, 0
+    syscall
