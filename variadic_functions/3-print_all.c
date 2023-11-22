1e77abe8ad0c949a7845959d1dd5ca48d47f909b
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-22 05:23:48 -0800

    3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+
+/**
+ * print_all - check the code
+ *
+ * @format: var
+ *
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str;
+	va_list ptr;
+
+	va_start(ptr, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ptr, int));
+				break;
+			case 'i':
+			       	printf("%d", va_arg(ptr, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double));
+				break;
+			case 's':
+				str = va_arg(ptr, char*);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				break;
+		}
+		if (format[i+1] && (format[i] == 'c' || format[i] == 'i'
+					|| format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		i++;
+	}
+}
