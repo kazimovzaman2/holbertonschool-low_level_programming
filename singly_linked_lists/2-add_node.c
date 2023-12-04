bbeca925d29f62e03a203c3e40aea6aedf115629
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-04 04:26:03 -0800

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - check the code
+ *
+ * @head: var
+ * @str: var
+ *
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
