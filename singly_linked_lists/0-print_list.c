c02186422842d15a45a5fed48e8cd5a7461dda8f
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-04 02:48:52 -0800

    Setup all configuration files.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_list - check the code
+ *
+ * @h: var
+ *
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+
+}
