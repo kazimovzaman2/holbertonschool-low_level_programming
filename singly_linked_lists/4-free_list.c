353fec65a2588aaaf422263e7385ce2e5adde0f4
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-04 05:15:23 -0800

    4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * free_list - check the code
+ *
+ * @head: var
+ *
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head;
+}
