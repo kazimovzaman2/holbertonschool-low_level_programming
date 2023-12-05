8b9f311ac1957b0ec628432d4edec273d540b4b2
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-05 03:08:56 -0800

    0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ *
+ * @h: var
+ *
+ * Return: Always 0.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
