cbaa5ff64422d2cf2da9c942d1cb7e945d031a83
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-08 07:41:16 -0800

    8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - check the code
+ *
+ * @head: var
+ * @index: var
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr = *head;
+	unsigned int i = 0;
+
+	if (index == 0)
+	{
+		*head = curr->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(curr);
+		return (1);
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		if (curr == NULL)
+			return (-1);
+		curr = curr->next;
+	}
+
+	if (curr == NULL)
+		return (-1);
+
+	if (curr->prev != NUL)
+		curr->prev->next = curr->next;
+	if (curr->next != NULL)
+		curr->next->prev = curr->prev;
+
+	free(curr);
+
+	return (1);
+}
