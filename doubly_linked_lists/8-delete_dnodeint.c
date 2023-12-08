6ee874948e4b56a360f89237bf0f2bbe572b3aeb
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-08 07:43:05 -0800

    8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -16,6 +16,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *curr = *head;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (-1);
+
 	if (index == 0)
 	{
 		*head = curr->next;
