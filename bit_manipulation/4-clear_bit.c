e206b46f66a7d4f2dc74ad4a00a24a355b7b324b
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-09 08:37:39 -0800

    4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - check the code
+ *
+ * @n: var
+ * @index: var
+ *
+ * Return: Always 0.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num = *n, num_to_remove = 1;
+	unsigned int size_ulong = sizeof(num) * 8, i = 0;
+
+	if (index < size_ulong)
+	{
+		if (((num >> index) & 1) == 1)
+		{
+			while (i != index)
+			{
+				num_to_remove *= 2;
+				i++;
+			}
+			num = num - num_to_remove;
+			*n = num;
+		}
+		return (1);
+	}
+	else
+		return (-1);
+}
