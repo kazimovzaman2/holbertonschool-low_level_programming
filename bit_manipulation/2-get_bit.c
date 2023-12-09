886a8a3d85f6e11e8be662fb01174d1165687b3f
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-09 08:13:16 -0800

    2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - check the code
+ *
+ * @n: var
+ * @index: var
+ *
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	return (n >> index) & 1;
+}
