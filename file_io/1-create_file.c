65bfa3dd702db96fbead26fcb3ac5be39e3e46f1
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-11 05:11:47 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	new_file = open(filename, O_CREAT | O_WRONLY, 0644);
+	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (new_file == -1)
 		return (-1);
 	if (!text_content)
