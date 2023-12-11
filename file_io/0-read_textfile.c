383adf2c0c8c4a9767a3d7d8d04c2a4ada390c00
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-11 04:28:54 -0800

    0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, l, w;
+	char *text;
+
+	text = malloc(letters);
+	if (!text || !filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+	l = read(fd, text, letters);
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+	return (w);
+}
