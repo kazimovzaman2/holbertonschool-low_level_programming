a6e687f2ce0f54315d14d52a360bffac6656f731
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-12 01:38:47 -0800

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	file = open(filename, O_WRONLY | O_APPEND);
 	wr = write(file, text_content, strlen(text_content));
-	
+
 	if (file == -1 || wr == -1)
 		return (-1);
 
