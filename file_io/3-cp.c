5b5d98f8f2b050074b45a48f901264e590b69ab8
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-12 02:53:45 -0800

    Change exit code and comment

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 		{
 			close(file);
 			close(new_file);
-			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 	}
