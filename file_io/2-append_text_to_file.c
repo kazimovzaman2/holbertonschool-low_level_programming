f8a3e6be436c040c94cf1eb4933079d1d47e23b2
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-12 01:10:17 -0800

    2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - check the code
+ *
+ * @filename: var
+ * @text_content: var
+ *
+ * Return: Always 0.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, wr;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	wr = write(file, text_content, strlen(text_content));
+
+	return (1);
+}
