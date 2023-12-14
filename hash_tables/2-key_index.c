4eee0cd6319920be9f59d8639f579ec4d466a529
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-14 05:04:06 -0800

    2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - check the code for
+ *
+ * @key: var
+ * @size: var
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_val = hash_djb2(key);
+
+	unsigned long int index = hash_val % size;
+	
+	return (index);
+}
