ae993ea773105af8242407030a032797ae0696b0
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-16 04:56:34 -0800

    0

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,20 +16,52 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *curr;
 
 	if (!key || !ht)
 		return (0);
 
 	i = key_index((unsigned char *)key, ht->size);
 
+	curr = ht->array[i];
+
+
+	while (curr)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			free(curr->value);
+			curr->value = strdup(value);
+			if (!curr->value)
+				return (0);
+			return (1);
+		}
+		curr = curr->next;
+	}
+
+
+
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 
 	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
 
-	strcpy(new_node->value, value);
+
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
 	new_node->next = ht->array[i];
 	ht->array[i] = new_node;
 
