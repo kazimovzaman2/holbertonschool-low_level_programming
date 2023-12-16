c6ca557be20f0119f43296b7ac169bc0aff6c8a2
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-16 04:40:52 -0800

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 
-	new_node->key = key;
+	new_node->key = strdup(key);
 
 	strcpy(new_node->value, value);
 	new_node->next = ht->array[i];
