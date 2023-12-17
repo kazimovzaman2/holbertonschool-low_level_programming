f72c5394dfaa6df214fa8db5c22a36b49cdc7843
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-16 20:35:23 -0800

    added comma flag

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -5,11 +5,9 @@
 
 
 /**
- * hash_table_set - check the code for
+ * hash_table_print - check the code for
  *
  * @ht: var
- * @key: var
- * @value: var
  *
  * Return: Always EXIT_SUCCESS.
  */
@@ -17,22 +15,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *curr;
+	int comma = 0;
 
 	if (!ht)
 		return;
 
 	printf("{");
-
 	for (i = 0; i < ht->size; i++)
 	{
 		curr = ht->array[i];
 
 		while (curr != NULL)
 		{
-			printf("'%s': '%s'", curr->key, curr->value);
-
-			if (curr->next != NULL)
+			if (comma)
 				printf(", ");
+			printf("'%s': '%s'", curr->key, curr->value);
+			comma = 1;
 
 			curr = curr->next;
 		}
