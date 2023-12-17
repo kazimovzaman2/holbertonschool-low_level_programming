2c0b933836f41379c828e80987e17d0fd58866f9
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-16 19:58:55 -0800

    4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+
+/*
+ * hash_table_get - function
+ *
+ * @ht: var
+ * key: var
+ *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *curr;
+
+	if (!ht || !key)
+		return (NULL);
+	i = key_index((unsigned char *)key, ht->size);
+	curr = ht->array[i];
+
+	return (curr->value);
+}
