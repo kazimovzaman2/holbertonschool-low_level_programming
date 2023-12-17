0fc7daa10b84756b20c6e9911a1114576d3b0a8d
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-16 20:02:31 -0800

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -21,5 +21,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	i = key_index((unsigned char *)key, ht->size);
 	curr = ht->array[i];
 
-	return (curr->value);
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+		curr = curr->next;
+	}
+
+	return (NULL);
 }
