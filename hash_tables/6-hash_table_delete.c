21430fdc0eba001fea58231fafb0bef69dd7611a
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-12-16 20:39:28 -0800

    6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: var
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr, *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr != NULL)
+		{
+			temp = curr->text;
+			free(curr->key);
+			free(curr->value);
+			free(curr);
+			curr = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
